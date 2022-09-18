c5d5867cc9506d44208dc92bd2ef69e04159bc69
Ta-Ching Chen <hello@tachingchen.com>
2022-09-19 01:19:08 +0800

    Update mnist.py

--- mnist.py
+++ mnist.py
@@ -1,3 +1,4 @@
+import os
 import tensorflow as tf
 mnist = tf.keras.datasets.mnist
 
@@ -15,5 +16,6 @@ model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-model.fit(x_train, y_train, epochs=5)
+# batch size: https://stackoverflow.com/a/52215772
+model.fit(x_train, y_train, batch_size=os.environ['BATCH_SIZE'], epochs=5)
 model.evaluate(x_test, y_test)
